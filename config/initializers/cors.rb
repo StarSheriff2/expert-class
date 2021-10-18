901b3e34e83b722eeb5b5fed3d04bd45238fa639
Francis Uloko <francisuloko@yahoo.com>
2021-10-18 23:50:46 +0100

    Configure sessions cookies aand cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,18 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins 'http://link_to_production'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
