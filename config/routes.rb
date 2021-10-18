f3bd8d4323c58f874b89324c605804b43df0f17e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-18 14:18:28 -0600

    Setup rspec tests and do one User request test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :user, :cities, :reservations, :courses
+      resources :users, :cities, :reservations, :courses
+      # get 'classes-list', to: 'classes#index'
     end
   end
 end
