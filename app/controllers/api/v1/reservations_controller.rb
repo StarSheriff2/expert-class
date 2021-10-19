560af591cecf977fc207030b33e47eccd8f3b023
Francis Uloko <francisuloko@yahoo.com>
2021-10-19 19:28:39 +0100

    Add error checks

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,7 +2,12 @@ class API::V1::ReservationsController < ApplicationController
   include CurrentUserConcern
 
   def index
-    @user_reservation =  @current_user.reservations.all
-    render json: @user_reservation
+    @user_reservation = Reservation.all
+    
+    if @user_reservation
+      render json: @user_reservation
+    else
+      render json: { status: 401 }
+    end
   end
 end
