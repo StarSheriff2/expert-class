7d7e5f814b39d2fc417d788468a7d93f7e7a36b0
Francis Uloko <francisuloko@yahoo.com>
2021-10-20 00:38:10 +0100

    Add Fake properties for reservation model

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :reservation do
-    name { Faker::Name.name }
+    association :user, :course, :city
+    date { '20-10-2021' }
+    user_id { rand(100) } 
+    course_id { rand(100) } 
+    city_id { rand(100) }
   end
 end
