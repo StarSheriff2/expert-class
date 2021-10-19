7db92fe4c10cfdcdc1d4aaec21e9d1b0a6a8c754
Francis Uloko <francisuloko@yahoo.com>
2021-10-20 00:42:36 +0100

    Fix linters errors

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :reservation do
     association :user, :course, :city
     date { '20-10-2021' }
-    user_id { rand(100) } 
-    course_id { rand(100) } 
+    user_id { rand(100) }
+    course_id { rand(100) }
     city_id { rand(100) }
   end
 end
