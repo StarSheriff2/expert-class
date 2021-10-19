2e8bb7dfaa43cc7bf42ba437ad85b8bcf44b85b3
Francis Uloko <francisuloko@yahoo.com>
2021-10-19 22:33:08 +0100

    Remove redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
       post :sign_in, to: 'sessions#create'
       delete :sign_out, to: "sessions#logout"
       get :signed_in, to: "sessions#logged_in"
-      
-      get '/api/v1/reservations', to: 'reservations#index'
     end
   end
   
