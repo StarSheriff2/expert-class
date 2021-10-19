2e3313aff7b062c1f8338f3b43ff9574bedca963
Breno Xavier <brenoxav@gmail.com>
2021-10-19 17:30:09 -0400

    Implement 'Courses' show endpoint, add tests

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,7 +1,22 @@
 class API::V1::CoursesController < ApplicationController
-  def index
-    @courses = Course.all
+  before_action :set_courses, only: [:index]
+  before_action :set_course, only: [:show]
 
+  def index
     render json: @courses
   end
+
+  def show
+    render json: @course
+  end
+end
+
+private
+
+def set_courses
+  @courses = Course.all
+end
+
+def set_course
+  @course = Course.find(params[:id])
 end
