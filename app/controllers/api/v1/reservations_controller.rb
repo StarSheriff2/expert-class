f8a83be5337c0fa9921a13209a717bdd3cdb1f29
Francis Uloko <francisuloko@yahoo.com>
2021-10-19 19:21:15 +0100

    Add reservation controller index action

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,8 @@
 class API::V1::ReservationsController < ApplicationController
+  include CurrentUserConcern
+
+  def index
+    @user_reservation =  @current_user.reservations.all
+    render json: @user_reservation
+  end
 end
