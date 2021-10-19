b728654f9cdca49818e0cc99bad56a3bf23f142d
Francis Uloko <francisuloko@yahoo.com>
2021-10-19 22:32:37 +0100

    Create and return reservations

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,13 +1,7 @@
-class API::V1::ReservationsController < ApplicationController
-  include CurrentUserConcern
-
+class API::V1::ReservationsController < ApplicationController  
   def index
-    @user_reservation = Reservation.all
-
-    if @user_reservation
-      render json: @user_reservation
-    else
-      render json: { status: 401 }
-    end
+    Reservation.create!({date: '12-09-2021', user_id: 1, course_id: 1, city_id: 1})
+    @user_reservation = Reservation.where(id: 1)
+    render json: @user_reservation
   end
 end
