b149272f04a90aebdde0dca45fa7b854d991bf49
Francis Uloko <francisuloko@yahoo.com>
2021-10-20 21:21:22 +0100

    Add reservation create endpoint

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -8,11 +8,25 @@ class API::V1::ReservationsController < ApplicationController
   def show
     render json: @reservation
   end
+  
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      render json: { message: 'User created successfully' }, status: 200
+    else
+      render json: { message: 'User create failed' }, status: 400
+    end
+  end
 
   private
 
   def set_reservation
     @user_reservations = Reservation.where(user_id: rand(2)) # add @current_user
     @reservation = Reservation.find_by(id: params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :user_id, :course_id, :city_id)
   end
 end
