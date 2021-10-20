3394252e193f3e1068f34e6774b546bac7f26ca7
Francis Uloko <francisuloko@yahoo.com>
2021-10-20 13:06:23 +0100

    Add association to reservation factory

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :reservation do
-    association :user, :course, :city
     date { '20-10-2021' }
-    user_id { rand(100) }
-    course_id { rand(100) }
-    city_id { rand(100) }
+    user
+    course
+    city
   end
 end
