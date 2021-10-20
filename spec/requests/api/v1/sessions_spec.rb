3d3fbb27c78c648f7098a89466191f279dffa07c
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-20 13:26:28 -0600

    Refactor the sessions_spec test

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'API::V1::Sessions', type: :request do
-  # initialize test data
-  let!(:users) { create_list(:user, 5) }
-  let(:existing_username) { { user: { username: users.first.username } } }
-  let(:non_existing_username) { { user: { username: 'unexistent_user' } } }
-
   describe 'POST /api/v1/sign_in' do
+    # initialize test data
+    let!(:users) { create_list(:user, 5) }
+    let(:existing_username) { { user: { username: users.first.username } } }
+    let(:non_existing_username) { { user: { username: 'unexistent_user' } } }
+
     context 'when the user exists in the database' do
       # make HTTP get request before each example
       before { post '/api/v1/sign_in', params: existing_username }
@@ -59,34 +59,63 @@ RSpec.describe 'API::V1::Sessions', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/signed_in' do
+    # initialize test data
+    let!(:user) { create_list(:user, 1) }
+    # before { allow(controller).to receive(:@current_user) { user } }
+
+    context 'when there is a session[:user_id] and user exists' do
+      # make HTTP get request before each example
+
+      before { get '/api/v1/signed_in' }
+      # let(:session_va) { session[users.first.id] }
+
+      it 'returns a json response' do
+        p "user: #{user}"
+        expect(json).not_to be_empty
+        expect(json.size).to be 2
+      end
+
+      xit 'creates a session variable with user id' do
+        expect(session[:user_id]).to eq(users.first.id)
+        expect(session[:user_id]).not_to eq(users.second.id)
+      end
+
+      xit 'returns correct user data' do
+        expect(json['user']['name']).to eq(users.first.name)
+        expect(json['user']['id']).to eq(users.first.id)
+        expect(json['user']['id']).not_to eq(users.second.id)
+      end
+
+      xit 'returns logged_in status as true' do
+        expect(json['logged_in']).to be true
+        expect(json['logged_in']).not_to be false
+      end
+
+      xit 'returns status code 200' do
+        expect(response).to have_http_status(200)
+        expect(response).not_to have_http_status(401)
+      end
+
+      xit 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when the user doesn\'t exist in the database' do
+      # make HTTP get request before each example
+      before { post '/api/v1/sign_in', params: non_existing_username }
+
+      xit 'returns a json response' do
+        expect(json).not_to be_empty
+        expect(json.size).to be 1
+      end
+
+      xit 'returns status code 401' do
+        expect(json['status']).to eq(401)
+      end
+    end
+  end
 end
 
-# describe 'POST /todos' do
-#   # valid payload
-#   let(:valid_attributes) { { title: 'Learn Elm', created_by: '1' } }
-
-#   context 'when the request is valid' do
-#     before { post '/todos', params: valid_attributes }
-
-#     it 'creates a todo' do
-#       expect(json['title']).to eq('Learn Elm')
-#     end
-
-#     it 'returns status code 201' do
-#       expect(response).to have_http_status(201)
-#     end
-#   end
-
-#   context 'when the request is invalid' do
-#     before { post '/todos', params: { title: 'Foobar' } }
-
-#     it 'returns status code 422' do
-#       expect(response).to have_http_status(422)
-#     end
-
-#     it 'returns a validation failure message' do
-#       expect(response.body)
-#         .to match(/Validation failed: Created by can't be blank/)
-#     end
-#   end
-# end
