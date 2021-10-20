58bd10a4385bacea8da170859e8c6159427b591e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-20 16:25:18 -0600

    Create signout endpoint

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe 'API::V1::Sessions', type: :request do
       end
 
       it 'creates a session variable with user id' do
+        p "session: #{session}"
         expect(session[:user_id]).to eq(users.first.id)
         expect(session[:user_id]).not_to eq(users.second.id)
       end
@@ -60,62 +61,30 @@ RSpec.describe 'API::V1::Sessions', type: :request do
     end
   end
 
-  # describe 'GET /api/v1/signed_in' do
-  #   # initialize test data
-  #   let!(:user) { create_list(:user, 1) }
-  #   let!(:@current_user) { }
-  #   # before { allow(controller).to receive(:@current_user) { user } }
+  describe 'DELETE /api/v1/sign_out' do
+    before { delete '/api/v1/sign_out' }
 
-  #   context 'when there is a session[:user_id] and user exists' do
-  #     # make HTTP get request before each example
+    it 'returns a json response' do
+      expect(json).not_to be_empty
+      expect(json.size).to be 2
+    end
 
-  #     before { get '/api/v1/signed_in' }
-  #     # let(:session_va) { session[users.first.id] }
+    it 'resets session variable' do
+      expect(session[:user_id]).to be_nil
+    end
 
-  #     it 'returns a json response' do
-  #       p "user: #{user}"
-  #       expect(json).not_to be_empty
-  #       expect(json.size).to be 2
-  #     end
+    it 'returns logged_out status as false' do
+      expect(json['logged_out']).to be true
+      expect(json['logged_out']).not_to be false
+    end
 
-  #     xit 'creates a session variable with user id' do
-  #       expect(session[:user_id]).to eq(users.first.id)
-  #       expect(session[:user_id]).not_to eq(users.second.id)
-  #     end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+      expect(response).not_to have_http_status(401)
+    end
 
-  #     xit 'returns correct user data' do
-  #       expect(json['user']['name']).to eq(users.first.name)
-  #       expect(json['user']['id']).to eq(users.first.id)
-  #       expect(json['user']['id']).not_to eq(users.second.id)
-  #     end
-
-  #     xit 'returns logged_in status as true' do
-  #       expect(json['logged_in']).to be true
-  #       expect(json['logged_in']).not_to be false
-  #     end
-
-  #     xit 'returns status code 200' do
-  #       expect(response).to have_http_status(200)
-  #       expect(response).not_to have_http_status(401)
-  #     end
-
-  #     xit 'returns http success' do
-  #       expect(response).to have_http_status(:success)
-  #     end
-  #   end
-
-  #   context 'when the user doesn\'t exist in the database' do
-  #     # make HTTP get request before each example
-  #     before { post '/api/v1/sign_in', params: non_existing_username }
-
-  #     xit 'returns a json response' do
-  #       expect(json).not_to be_empty
-  #       expect(json.size).to be 1
-  #     end
-
-  #     xit 'returns status code 401' do
-  #       expect(json['status']).to eq(401)
-  #     end
-  #   end
-  # end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
