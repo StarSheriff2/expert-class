6a5ecab94033051c6267bce066ba46160efc48d3
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-22 13:33:53 -0600

    Delete session_store file and edit development.rb in config/enviroments

--- config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-# if Rails.env == "production"
-#   Rails.application.config.session_store :cookie_store, key: '_expert-class', domain: "https://link-to-your-production-app.com/"
-# else
-#   Rails.application.config.session_store :cookie_store, key: '_expert-class'
-# end
