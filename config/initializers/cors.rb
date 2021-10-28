61d6e98fe202d1c404015de693290f034c8b2bcc
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 09:57:04 -0600

    Edit Cors file for testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,20 +2,19 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+  # allow do
+  #   origins 'https://expert-class-frontend.netlify.app'
 
-  allow do
-    origins 'https://expert-class-frontend.netlify.app'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
 end
