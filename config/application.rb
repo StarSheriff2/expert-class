dbc29609f3cc3a3b85ab1d469e85fad00067b287
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 12:01:23 -0600

    Change domain in applcation.rb file top heroku URL

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module ExpertClassBackend
     config.middleware.use ActionDispatch::Session::CookieStore,
     #========= Production Setup for Heroku ==============#
     key: '_export_class',
-    domain: :all,
+    domain: 'https://expert-class-backend.herokuapp.com',
     secure: true,
     same_site: 'None'
   end
