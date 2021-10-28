51fe813a865b467d57b25883127514716754d9dc
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 13:23:35 -0600

    Remove CSRF Token verification settings from app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,28 @@
 class ApplicationController < ActionController::API
-  # skip_before_action :verify_authenticity_token
-  before_action :set_csrf_cookie
+  skip_before_action :verify_authenticity_token
+  # before_action :set_csrf_cookie
 
   include ActionController::Cookies
-  include ActionController::RequestForgeryProtection
+  # include ActionController::RequestForgeryProtection
 
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   include Response
   include ExceptionHandler
 
-  def cookie
-    'ok'
-  end
+  # def cookie
+  #   'ok'
+  # end
 
-  private
+  # private
 
-  def set_csrf_cookie
-    cookies['CSRF-TOKEN'] = {
-      value: form_authenticity_token,
-      domain: 'https://expert-class-backend.herokuapp.com'
-      #========= Production Setup for Heroku ==============#
-      # same_site: 'None',
-      # secure: true
-    }
-  end
+  # def set_csrf_cookie
+  #   cookies['CSRF-TOKEN'] = {
+  #     value: form_authenticity_token,
+  #     domain: 'https://expert-class-backend.herokuapp.com'
+  #     #========= Production Setup for Heroku ==============#
+  #     # same_site: 'None',
+  #     # secure: true
+  #   }
+  # end
 end
