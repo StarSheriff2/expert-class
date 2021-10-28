b5112ea0fe91b8fa6542a5e2534fbf5aca47289d
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 10:01:58 -0600

    Change cors Credentials settings to false for testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: false
   end
   # allow do
   #   origins 'https://expert-class-frontend.netlify.app'
