dae317a385149374a24c3e84901fcae4db5bb101
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 11:29:18 -0600

    Change same_site seetings in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,10 @@ class ApplicationController < ActionController::API
   def set_csrf_cookie
     cookies['CSRF-TOKEN'] = {
       value: form_authenticity_token,
-      domain: 'https://expert-class-backend.herokuapp.com',
+      domain: 'https://expert-class-backend.herokuapp.com'
       #========= Production Setup for Heroku ==============#
-      same_site: 'None',
-      secure: true
+      # same_site: 'None',
+      # secure: true
     }
   end
 end
