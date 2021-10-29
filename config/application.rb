1a96d6395bd19ce5bd0280d90356a601982fb5ef
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 22:38:11 -0600

    Test 6

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module ExpertClassBackend
     config.middleware.use ActionDispatch::Session::CookieStore,
     #========= Production Setup for Heroku ==============#
     # key: '_expert_class',
-    domain: 'netlify.app'
+    domain: 'expert-class-frontend-v2.netlify.app'
     # secure: true,
     # same_site: 'None'
   end
