1c483c5ce981d80a147082b2802b1fac5a3e6427
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 23:21:30 -0600

    Test 18

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # end
 
   allow do
-    origins 'expert-class-frontend-v2.netlify.app'
+    origins 'expert-class-frontend-v2.netlify.app', 'https://expert-class-frontend-v2.netlify.app'
 
     resource '*',
       headers: :any,
