69c9fbd4b0b6982628d0c3ddf5608768eeaf2342
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 22:19:31 -0600

    Change domain in applcaitionController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,10 +19,10 @@ class ApplicationController < ActionController::API
   def set_csrf_cookie
     cookies['CSRF-TOKEN'] = {
       value: form_authenticity_token,
-      domain: 'https://expert-class-frontend-v2.netlify.app',
+      domain: 'netlify.app'
       #========= Production Setup for Heroku ==============#
-      same_site: 'None',
-      secure: true
+      # same_site: 'None',
+      # secure: true
     }
   end
 end
