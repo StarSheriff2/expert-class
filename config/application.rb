5b3c17d254bf3823f4dd6ba7768ad65be0c2b27f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-28 22:43:58 -0600

    Test 8

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module ExpertClassBackend
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
+    # in config/environments, which are processed later..
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
