3ecf1482471a61a95cd9ef956f51b1e6c2c9eec4
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-02 17:35:48 -0600

    Test 59

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,4 @@
-Rails.application.config.action_controller.forgery_protection_origin_check = false
+# Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
