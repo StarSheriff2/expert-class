f17f90fbef132aaa50d3078578b88a9a45c0fda2
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-02 15:13:30 -0600

    Test 30

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'localhost:3000'
 
     resource '*',
       headers: :any,
