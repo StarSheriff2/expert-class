392ae3c61dc4ce0736a84a23d278cb83fbeab232
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-02 15:08:59 -0600

    Edit applciation controller settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,14 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins 'http://localhost:3000'
+  allow do
+    origins 'http://localhost:3000'
 
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 
   allow do
     origins 'expert-class-frontend-v2.netlify.app', 'https://expert-class-frontend-v2.netlify.app'
