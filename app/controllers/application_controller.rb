e4a254166c8374d00820af0173367fb416be8ce5
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-02 17:21:35 -0600

    Test 53

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,8 @@ class ApplicationController < ActionController::API
   def set_csrf_cookie
     cookies['CSRF-TOKEN'] = {
       value: form_authenticity_token,
-      # domain: 'expert-class-backend.herokuapp.com',
-      domain: 'expert-class-frontend-v2.netlify.app',
+      domain: 'expert-class-backend.herokuapp.com',
+      # domain: 'expert-class-frontend-v2.netlify.app',
       #========= Production Setup for Heroku ==============#
       same_site: 'Lax',
       secure: true
