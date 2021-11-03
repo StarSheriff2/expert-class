8fc6a20732e4362ee54879b2ed9a123ffb1738b1
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-03 14:39:38 -0600

    Test 65

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   # skip_before_action :verify_authenticity_token
   before_action :set_csrf_cookie
 
