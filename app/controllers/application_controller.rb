c06956318519f8a3ba4130c4647a34fcc3a0fd3e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-03 17:37:14 -0600

    test 72

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
   include ActionController::Cookies
   # include ActionController::RequestForgeryProtection
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
 
   include Response
