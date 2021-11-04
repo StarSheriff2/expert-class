232cfc1532ce97c4d18d2099f6ff2380903e604f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-04 15:15:19 -0600

    test 81

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,6 +6,6 @@ module CurrentUserConcern
   end
 
   def set_current_user
-    @current_user = User.find(session[:user_id]) if session[:user_id]
+    @current_user = User.find(session[:_expert_class]) if session[:_expert_class]
   end
 end
