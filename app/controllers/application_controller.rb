7acdd495b1e3e1ed7e872e1102003a0f683e44b0
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-05 10:21:25 -0600

    test 88

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::API
 
   before_action :set_current_user
 
+  private
+
   def set_current_user
     @current_user = User.find(session[:user_id]) if session[:user_id]
   end
