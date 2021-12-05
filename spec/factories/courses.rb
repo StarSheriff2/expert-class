5a038a96570e8d13b0195552cda4dd704ad613ef
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-12-05 00:35:03 -0600

    Fix error with sessions test for logout action

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -4,11 +4,6 @@ FactoryBot.define do
     description { Faker::Lorem.paragraph }
     instructor { Faker::Name.name }
     duration { rand(12) }
-    # let(:path_to_file) { Rails.root.join 'spec/fixtures/file.csv' }
     image { Rack::Test::UploadedFile.new Rails.root.join('spec', 'test_images', 'instructor1.jpeg'), 'image/jpeg' }
-    # image { Faker::Internet.url(host: 'faker', path: '/fake_test_path', scheme: 'https') }
-    # image { File.open(Dir.glob(File.join(Rails.root, 'spec/test_images', '*')).sample) }
-    # image { Rails.root.join('spec', 'test_images', 'instructor1.jpeg') }
-    # course_image_url { ActionDispatch::Http::UploadedFile.new("#{Rails.root}/spec/test_images/instructor1.jpeg") }
   end
 end
