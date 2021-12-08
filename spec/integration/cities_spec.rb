50111c96f7f065c8b8e91d752fa8ffdabf5b416a
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-12-08 15:31:11 -0600

    Create api-docs for /cities endpoint with swagger gem

--- spec/integration/cities_spec.rb
+++ spec/integration/cities_spec.rb
@@ -1,30 +1,40 @@
 require 'swagger_helper'
 
+TAGS_CITY = 'Cities'.freeze
+
 describe 'Cities API' do
   let!(:user) { create(:user) }
   let!(:existing_username) { { user: { username: user.username } } }
+
+  after do |example|
+    example.metadata[:response][:content] = {
+      'application/json' => {
+        example: JSON.parse(response.body, symbolize_names: true)
+      }
+    }
+  end
 
   before { post '/api/v1/sign_in', params: existing_username }
 
   path '/api/v1/cities' do
     get 'returns a list of all cities' do
-      tags 'Cities'
+      tags TAGS_CITY
       produces 'application/json'
 
       response '200', 'success' do
-        schema type: :array, items: {
-          type: :object
-        }
-        examples 'application/json' => [
-          {
-            id: 1,
-            name: Faker::Nation.capital_city
-          },
-          {
-            id: 2,
-            name: Faker::Nation.capital_city
-          }
-        ]
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   name: { type: :string },
+                   created_at: { type: :string },
+                   updated_at: { type: :string }
+                 },
+                 required: %w[id name created_at updated_at]
+               }
+
+        let!(:cities) { create_list(:city, 5) }
 
         run_test!
       end
