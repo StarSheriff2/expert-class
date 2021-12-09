82147897c54b8f8676b5d396377c500d2d2a20e7
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-12-08 22:22:50 -0600

    Create lambda to vary the string for the image_url

--- spec/integration/courses_spec.rb
+++ spec/integration/courses_spec.rb
@@ -4,7 +4,6 @@ TAGS_COURSE = 'Courses'.freeze
 
 describe 'Courses API' do
   let!(:courses) { create_list(:course, 5) }
-  let!(:image_url) { "https://res.cloudinary.com/starsheriff/image/upload/#{Faker::Lorem.characters(number: 10)}.jpeg" }
 
   # let!(:course_id) { courses.first.id }
   # let!(:deleted_course) { courses.first }
@@ -12,9 +11,14 @@ describe 'Courses API' do
   let!(:existing_username) { { user: { username: user.username } } }
 
   # Stub cloudinary related methods
+  image_url = ->(k) { "https://res.cloudinary.com/starsheriff/image/upload/#{k}.jpeg" }
+  generate_random_key = -> { Faker::Lorem.characters(number: 10) }
+
   before do
     allow_any_instance_of(Course).to receive(:attach_image)
-    allow_any_instance_of(Course).to receive(:course_image_url).and_return(image_url)
+    allow_any_instance_of(Course).to receive(:course_image_url) do
+      image_url.call(generate_random_key.call)
+    end
   end
 
   before { post '/api/v1/sign_in', params: existing_username }
